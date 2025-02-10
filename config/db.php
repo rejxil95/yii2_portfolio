389185ba0d4ff8f8781f883d8619154ae345173f
rejxil95 <rachel_fitz95@hotmail.com>
2025-02-10 21:47:44 +0100

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php 
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_portfolio',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+?>
---
+++ config/main.php
@@ -0,0 +1,4 @@
+'components' => [
+    'db' => require __DIR__ . '/db.php',
+],
+
