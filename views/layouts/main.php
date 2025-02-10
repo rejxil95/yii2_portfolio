17ef2228c91b1f57aa2149df0fba5cbd34d1bdf9
rejxil95 <rachel_fitz95@hotmail.com>
2025-02-10 22:05:52 +0100

    :)

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,6 +23,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
+    
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
